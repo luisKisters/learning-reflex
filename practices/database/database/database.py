b1fe31b6f0172b571d21a3dc9e1cb1a8b3ad04a0
luisKisters <luis.w.kisters@gmail.com>
2024-10-16 16:21:23 +0200

    final touches on database stuff

--- practices/database/database/database.py
+++ practices/database/database/database.py
@@ -5,10 +5,17 @@ import reflex as rx
 from rxconfig import config
 
 
+class Product(rx.Model, table=True):
+    name: str
+    description: str
+    price: float
+
+
 class State(rx.State):
     name: str
     description: str
     price: float
+    products: list[Product] = []
 
     def add_product(self):
         with rx.session() as session:
@@ -22,11 +29,9 @@ class State(rx.State):
             session.commit()
             print("Product added, name: ", self.name)
 
-
-class Product(rx.Model, table=True):
-    name: str
-    description: str
-    price: float
+    def get_products(self):
+        with rx.session() as session:
+            self.products = session.exec(Product.select()).all()
 
 
 def index() -> rx.Component:
@@ -37,6 +42,14 @@ def index() -> rx.Component:
         rx.input(value=State.description, on_change=State.set_description),
         rx.input(value=State.price, on_change=State.set_price),
         rx.button("add product", on_click=State.add_product()),
+        rx.heading("products"),
+        rx.foreach(
+            State.products,
+            lambda product: rx.text(
+                f"{product.name} {product.description} {product.price}"
+            ),
+        ),
+        rx.button("get products", on_click=State.get_products()),
     )
 
 
